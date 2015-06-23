46051cf1e8dd9dfc580b8c9019ae0c35b7f4bfe8
Isaac Hinman <isaacshinman@gmail.com>
2015-06-23 14:11:40 +0100

    Added APOD iFrame Support
    NASA’s APOD (Astronomy Picture of the Day) is actually sometimes a
    video, typically from YouTube or Vimeo. An if/elseif clause has been
    added to create appropriate DOM elements depending on media type.

--- js/angular-app.js
+++ js/angular-app.js
@@ -6,7 +6,6 @@ var spaceVis = angular.module('prototype', ['ngRoute']);
 
 /* CREATE FACTORY TO PORT RENDERER INTO CONTROLLERS */
 
-
 /* CONFIGURE ROUTES */
 spaceVis.config(function($routeProvider, $locationProvider) {
     $routeProvider
@@ -44,36 +43,177 @@ spaceVis.config(function($routeProvider, $locationProvider) {
 });
 /* END CONFIGURE ROUTES */
 
-/* MAIN CONTROLLER */
+/*********************************************************/
+/******************** STAR CONTROLLER ********************/
+/*********************************************************/
 spaceVis.controller('starFinderController', function($scope) {
-});
-/* END MAIN CONTROLLER */
 
-/* EARTH CONTROLLER */
+});
+/* END STAR CONTROLLER */
+
+/*********************************************************/
+/******************** EARTH CONTROLLER *******************/
+/*********************************************************/
 spaceVis.controller('earthController', function($scope) {
+
 });
 /*END EARTH CONTROLLER */
 
-/* APOD CONTROLLER */
+/*********************************************************/
+/******************** APOD CONTROLLER ********************/
+/*********************************************************/
 spaceVis.controller('apodController', function($scope) {
 
     $scope.load = function() {
 
-        $("#lightSlider").lightSlider({
-            item: 1,
-            adaptiveHeight: true,
-            enableTouch: true,
-            enableDrag: true,
-            loop: false,
-            speed: 200,
-            keyPress: true,
-            slideMargin: 0
+        // MONTH NAMES
+        var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-        });
+        /////////////////////////////////
+        //////// GET INTIAL APODS ///////
+        /////////////////////////////////
+            function slideFunction() {
+                window.imgIds = [ 'apod1', 'apod2', 'apod3', 'apod4', 'apod5', 'apod6', 'apod7', 'apod8', 'apod9', 'apod10' ];
+                for (var i = 0; i < window.imgIds.length; i++) {
+                    (function(id) {
 
-        $('#lightSlider').onAfterSlide.refresh;
+                        // GENERATE DATE
+                        var date = new Date();
+                        date.setDate(date.getDate() - i);
+                        var day = date.getDate();
+                        var month = ("0" + (date.getMonth() +1)).slice(-2);
+                        var monthName = date.getMonth();
+                        console.log(month);
+                        var year = date.getFullYear();
 
-    }
+                        // GENERATE XML REQUEST
+                        var apodUrl = "https://api.nasa.gov/planetary/apod?concept_tags=True&date=" + year + "-" + month + "-" + day + "&api_key=5iF1Ge5myl5KDyqPuyZ1XxQyAMCNxbCt0dlR3M7R";
+                        console.log("https://api.nasa.gov/planetary/apod?concept_tags=True&date=" + year + "-" + month + "-" + day + "&api_key=5iF1Ge5myl5KDyqPuyZ1XxQyAMCNxbCt0dlR3M7R");
+                        var apodXml = new XMLHttpRequest();
+                        apodXml.open('GET', apodUrl, true);
+                        apodXml.send(null);
+
+                        // WHEN REQUEST IS READY, ADD IMG SRC
+                        apodXml.onreadystatechange=function() {
+                            if (apodXml.readyState==4 && apodXml.status==200) {
+                                var apodParse = JSON.parse(apodXml.responseText);
+                                document.getElementById(id).src = apodParse.url;
+                                document.getElementById(id+"Date").innerHTML = day + " " + monthNames[monthName] + " " + year;
+                            }
+                        }
+                    })(window.imgIds[i]);
+                }
+            }
+
+        slideFunction();
+
+        ////////////////////////////////////
+        // GENERATE MORE SLIDES AS NEEDED //
+        ////////////////////////////////////
+
+            function generateNewSlides() {
+
+                var stopGeneratingSlides = (window.imgIds.length + 10);
+
+                var startLength = (window.imgIds.length + 1);
+
+                for (var i = startLength; i < stopGeneratingSlides; i++) {
+
+                    // SET UP NEW SLIDE HTML
+                    var li = '<li id="apodLi'+i+'"></li>';
+                    $('#lightSlider').append(li);
+                    window.imgIds.push('apod'+i);
+
+                    // GENERATE XML REQUEST
+                    function slideSetUp(callback) {
+
+                        // GENERATE DATE
+                        var date = new Date();
+                        date.setDate(date.getDate() - (i-1));
+                        var day = date.getDate();
+                        var month = ("0" + (date.getMonth() +1)).slice(-2);
+                        var monthName = date.getMonth();
+                        var year = date.getFullYear();
+
+                        var apodUrl = "https://api.nasa.gov/planetary/apod?concept_tags=True&date=" + year + "-" + month + "-" + day + "&api_key=5iF1Ge5myl5KDyqPuyZ1XxQyAMCNxbCt0dlR3M7R";
+                        var apodXml = new XMLHttpRequest();
+                        apodXml.open('GET', apodUrl, true);
+                        apodXml.send(null);
+
+                        // WHEN REQUEST IS READY, ADD IMG SRC
+                        apodXml.onreadystatechange=function() {
+                            if (apodXml.readyState==4 && apodXml.status==200) {
+                                var apodParse = JSON.parse(apodXml.responseText);
+                                callback(apodParse);
+                            }
+                        }
+                    }
+
+                        slideSetUp(
+                            (function(i, day, monthName, year) {
+                                return function(result) {
+
+                                    var mediaType = result.media_type;
+
+                                    function createDOM() {
+                                        console.log(mediaType);
+                                        if (mediaType == "image") {
+                                            document.getElementById("apodLi"+i).innerHTML += '<img id="apod' + i + '" class="rounded-corners apod-image"><br><span id="apod' + i + 'Date"></span>';
+                                        } else if (mediaType == "video") {
+                                            document.getElementById("apodLi"+i).innerHTML += '<iframe id="apod' + i + '" class="apod-video" frameBorder="0"></iframe><br><span id="apod' + i + 'Date"></span>';
+                                            /*
+                                            var newSlide = '<li><iframe id="apod' + i + '" class="apod-video"></iframe><br><span id="apod' + i + 'Date"></span></li>';
+                                            $('#lightSlider').append(newSlide);
+                                            */
+                                        }
+                                    }
+
+                                    function fillDOM() {
+                                        // GENERATE DATE
+                                        var date = new Date();
+                                        date.setDate(date.getDate() - (i-1));
+                                        var day = date.getDate();
+                                        var monthName = date.getMonth();
+                                        var year = date.getFullYear();
+                                        document.getElementById('apod'+i).src = result.url;
+                                        document.getElementById('apod'+i+"Date").innerHTML = day + " " + monthNames[monthName] + " " + year;
+                                    }
+
+                                    createDOM();
+                                    fillDOM();
+                                }
+                            })(i)
+                        );
+                }
+            }
+
+            // SET UP SLIDER
+            window.slider = $("#lightSlider").lightSlider({
+                    item: 1,
+                    adaptiveHeight: true,
+                    enableTouch: true,
+                    enableDrag: true,
+                    loop: false,
+                    speed: 0,
+                    keyPress: true,
+                    slideMargin: 0,
+                    gallery: false,
+
+                    // AFTER EACH SLIDE TRANSITION, DO THIS
+                    onAfterSlide: function (el) {
+
+                        // WHEN GETTING CLOSE TO END OF SLIDES, ADD MORE
+                        currentSlide = '#apod' + (window.imgIds.length - 3);
+
+                        if ($(currentSlide).parent().hasClass("active")) {
+                            generateNewSlides();
+                            window.slider.refresh();
+                        };
+
+                    },
+
+                });
+            }
 
 });
 /* END APOD CONTROLLER */
